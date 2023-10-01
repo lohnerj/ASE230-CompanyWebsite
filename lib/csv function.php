df87dcf2db9ba4c026dd93362ccbd8ffb75d1768
NatLevesque <Redlegsfreak@gmail.com>
2023-10-01 19:56:38 -0400

    changes

--- lib/csv function.php
+++ lib/csv function.php
@@ -1,8 +1,15 @@
-$Open = fopen("info.csv", "r");
-$data = fgetcsv($Open, 1000, ",");
-while (($data = fgetcsv($Open, 1000, ",")) !== FALSE) 
-{
-  $array[] = $data    
+<?php
+$file='info.csv';
+$fp=fopen($file, 'r');
+while(!feof($fp)) {
+  $content=fgets($fp);
+  $award=explode('", ', $content);
+  echo "<ul>";
+    echo "<li>$award[0] </li>";
+    echo "<li>$award[1] </li>";
+  echo "</ul>";
+  '<br >';
 }
-
-fclose($Open);
+fclose($fp);
+echo '<hr>';
+?>
